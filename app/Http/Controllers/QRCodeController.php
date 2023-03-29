950fc46afe5f711694c198c13d8da0e1d7b9b03c
ludwig2k <elfytheengie1337@gmail.com>
2023-03-29 03:52:00 -0300

    comments to controller

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -1,11 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-use Endroid\QrCode\Builder\Builder;
-use Endroid\QrCode\Encoding\Encoding;
-use Endroid\QrCode\Label\Label;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
@@ -17,24 +12,33 @@ class QRCodeController extends Controller
     $linkedin_url = $request->input('linkedin_url');
     $github_url = $request->input('github_url');
 
+    // Construct an array with input data
     $data = [
         'name' => $name,
         'linkedin_url' => $linkedin_url,
         'github_url' => $github_url
     ];
 
+    // Convert the data array to JSON and encode it to base64
     $json = json_encode($data);
     $encodedData = base64_encode($json);
+
+    // Create a route for the decoded data and generate QR Code with the URL
     $url = route('qr_code_data', $encodedData);
     $qrCode = QrCode::size(250)->generate($url);
+
+    // Return the view with QR Code
     return view('qr-code', compact('qrCode'));
 }
     
 
 public function showData($data)
 {
+    // Decode the base64 encoded data and convert it to an array
     $decodedData = base64_decode($data);
     $data = json_decode($decodedData, true);
+
+    // Return the view with contact information
     return view('contact-info', compact('data'));
 }
 
